c23f05b74541b7c5ea8f85b3d9e4efa19a556f25
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2024-01-08 10:37:18 +0530

    import java.util.*; public class DiskCapacity {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         float b = a*2.54f;         System.out.printf("%.2f",b);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class DiskCapacity
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b = a*2.54f;
+        System.out.printf("%.2f",b);
+    }
+}
