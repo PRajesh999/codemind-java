8c9e284edf4117e0d649421c4979bf8119a7d3d7
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2024-01-11 22:15:24 +0530

    import java.util.*; import java.lang.Math; public class Hypo {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int c = a*a;         int d = 4*a;         System.out.printf("%d %d",c,d);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math;
+public class Hypo
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = a*a;
+        int d = 4*a;
+        System.out.printf("%d %d",c,d);
+    }
+}
