5ba0700c7dd51279f3a169a005d8fc05112c621a
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2024-01-11 22:18:21 +0530

    import java.util.*; import java.lang.Math; public class Hypo {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         float a = sc.nextFloat();         float c = sc.nextFloat();         float d = a*c;         System.out.printf("%.2f",d);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math;
+public class Hypo
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float c = sc.nextFloat();
+        float d = a*c;
+        System.out.printf("%.2f",d);
+    }
+}
