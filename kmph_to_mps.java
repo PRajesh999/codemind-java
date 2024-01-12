f8f9ee054fe8fabdd948078c8a4cf91f03cb794f
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2024-01-12 11:20:04 +0530

    import java.util.*; public class DiskCapacity {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         double b = (a*5)/18d;         System.out.printf("%.2f",b);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class DiskCapacity
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = (a*5)/18d;
+        System.out.printf("%.2f",b);
+    }
+}
