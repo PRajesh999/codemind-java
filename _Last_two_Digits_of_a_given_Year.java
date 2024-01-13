c71475486417067b72bc5d2d0132149f2c41cf00
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2024-01-13 21:52:21 +0530

    import java.util.*; public class Solution {
        public static void main(String[] args) {
            Scanner scan = new Scanner(System.in);
            int a = scan.nextInt();
            int b = a%100;
            System.out.printf("%02d",b);
        }
    }

---
+++ _Last_two_Digits_of_a_given_Year.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = a%100;
+        System.out.printf("%02d",b);
+    }
+}
